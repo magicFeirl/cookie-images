f1dd75a1ba9b3a7c06b673754cd070f3968b2559
magicFeirl <2100709458@qq.com>
2026-09-21 21:58:49 +0800

    fix: 单页数据没有更新的问题

--- cookie-images-backend/run_crawler.py
+++ cookie-images-backend/run_crawler.py
@@ -12,13 +12,17 @@ for uid in UID_LIST:
 
     for page in range(MAX_CRAW_DRAW_PAGE):
         items = crawler.fetch_draw_doc_list(uid=uid, page_num=page) or []
-        count += len(items)
+        
+        if items:
+            count += len(items)
+            print(items[0])
+        
+        DrawTable.insert_many(items)
 
         if not items or page == MAX_CRAW_DRAW_PAGE - 1:
             print(f"UID: {uid} 爬取完毕")
             print(f"uid={uid} 入库/更新 {count} 条数据")
             break
 
-        DrawTable.insert_many(items)
 
         print(f"Crawling {uid} no.{page} page data")
